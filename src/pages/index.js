6b82c82206a743a0b2060341c9498fbcf72947fe
FluffyKas <maruzs.zso@gmail.com>
2022-06-27 14:06:06 +0200

    chore: add export

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 export { About } from "./about/About";
 export { Catalogue } from "./catalogue/Catalogue";
 export { Order } from "./order/Order";
+export { ProductPage } from "./productPage/ProductPage";
 export { AppRoutes } from "./AppRoutes";
