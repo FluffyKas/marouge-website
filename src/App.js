ced29ecec19b4a3e15bc09cb32cd5d724cbbc9d5
FluffyKas <maruzs.zso@gmail.com>
2022-06-27 12:04:11 +0200

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import { Header } from "./components";
+import { AppRoutes } from "./pages";
+
 function App() {
+
+  const location = useLocation();
+
   return (
     <div className="App">
-      stuff
+      <Header />
+      <main>
+        <Routes location={location} key={location.pathname}>
+          {AppRoutes.map((route, index) => {
+            const { path, element } = route;
+            return <Route path={path} element={element} key={index} />;
+          })}
+        </Routes>
+      </main>
 
     </div>
   );
