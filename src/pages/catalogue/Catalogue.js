333cd91d90c839d7a990ac0012d871d81085d8c9
FluffyKas <maruzs.zso@gmail.com>
2022-06-27 13:05:10 +0200

    feat: add mock data and basic styles

--- src/pages/catalogue/Catalogue.js
+++ src/pages/catalogue/Catalogue.js
@@ -1,5 +1,47 @@
+import logo from "../../assets/images/logo.jpg";
+import "./Catalogue.scss";
+
 export const Catalogue = () => {
   return (
-    <div>Katalogus</div>
+    <div className="catalogue-container">
+      <h1>Termekek</h1>
+      <div className="product-grid">
+        <article className="product">
+          <img src={logo} alt="Marouge" />
+          <h2>Termek neve</h2>
+          <p>Termek ara</p>
+        </article>
+        <article className="product">
+          <img src={logo} alt="Marouge" />
+          <h2>Termek neve</h2>
+          <p>Termek ara</p>
+        </article>
+        <article className="product">
+          <img src={logo} alt="Marouge" />
+          <h2>Termek neve</h2>
+          <p>Termek ara</p>
+        </article>
+        <article className="product">
+          <img src={logo} alt="Marouge" />
+          <h2>Termek neve</h2>
+          <p>Termek ara</p>
+        </article>
+        <article className="product">
+          <img src={logo} alt="Marouge" />
+          <h2>Termek neve</h2>
+          <p>Termek ara</p>
+        </article>
+        <article className="product">
+          <img src={logo} alt="Marouge" />
+          <h2>Termek neve</h2>
+          <p>Termek ara</p>
+        </article>
+        <article className="product">
+          <img src={logo} alt="Marouge" />
+          <h2>Termek neve</h2>
+          <p>Termek ara</p>
+        </article>
+      </div>
+    </div>
   );
 }
--- src/pages/order/Order.js
+++ src/pages/order/Order.js
@@ -1,5 +1,11 @@
+import "./Order.scss";
+
 export const Order = () => {
   return (
-    <div>Rendeles</div>
+    <div className="order-container">
+      <h1>Rendeles modja</h1>
+      <p>Kulgyel zsozsot, a csomag galambbal erkezik 3 munkanapon belul. Easy.</p>
+      <p>Szallitasi dij: egy marek mag kozvetlenul a kezbesitonek.</p>
+    </div>
   );
 }
