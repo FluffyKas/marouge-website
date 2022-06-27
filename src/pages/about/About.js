dca938a41b6c68cacff43d12de3c873e7aa6fc97
FluffyKas <maruzs.zso@gmail.com>
2022-06-27 13:48:25 +0200

    style: profile pic

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,8 +1,10 @@
 import "./About.scss";
+import profilePic from "../../assets/images/profile-pic.jpg";
 
 export const About = () => {
   return (
     <div className="about-container">
+      <img src={profilePic} alt="" className="profile-pic" />
       <h1 className="name">Maruzs Luco</h1>
       <p className="text">Maruzs Luci vagyok, egy nagyon ugyes kismacska. Mindenfelet varrok es rajzolok. Szeretem a termeszetes es hazai cuccokat, mert egy environmentally conscious kiscica vagyok. Ha osztod a nezeteimet es tetszenek a holmijaim, rendelj valamit pls :3 </p>
     </div>
