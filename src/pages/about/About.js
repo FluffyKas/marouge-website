5880741f7e92ea20ba24612c6717afa0254ccd78
FluffyKas <maruzs.zso@gmail.com>
2022-06-27 12:51:35 +0200

    feat: add mock text and basic styles

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,5 +1,10 @@
+import "./About.scss";
+
 export const About = () => {
   return (
-    <div>Info Lucikarol</div>
+    <div className="about-container">
+      <h1 className="name">Maruzs Luco</h1>
+      <p className="text">Maruzs Luci vagyok, egy nagyon ugyes kismacska. Mindenfelet varrok es rajzolok. Szeretem a termeszetes es hazai cuccokat, mert egy environmentally conscious kiscica vagyok. Ha osztod a nezeteimet es tetszenek a holmijaim, rendelj valamit pls :3 </p>
+    </div>
   );
 }
