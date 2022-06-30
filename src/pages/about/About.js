0b4c35b2326eeb117e1f404b5718314065c00baa
FluffyKas <maruzs.zso@gmail.com>
2022-06-30 13:18:42 +0200

    fix: basic formatting

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -6,14 +6,13 @@ export const About = () => {
     <div className="about-container">
       <img src={profilePic} alt="" className="profile-pic" />
       <h1 className="name">Marouge</h1>
-      <h2>varrott és rajzolt holmik</h2>
-      <p className="text">Maruzs Luca vagyok, a márka kitalálója, megalkotója, varrónője, grafikusa és mindenese.
-        Évek óta álmodozom róla, hogy egyszer elinduljak ezen az úton, és végre azzal foglalkozzak, amit ennyire szeretek.
-        Egy pár éve elkerülhetetlenné vált, hogy elkezdjek átváltani egy (legalább kicsit) környezettudatosabb életmódra. Itt jött be a képbe az alapvető személyiségjegyem, hogy „csináljunk meg mindent házilag, ha lehet”. Nagyjából egy éve nekikezdtem a varrásnak, azonnal megszerettem, és azóta is csak keresem és próbálgatom az újabb, hasznosabb és praktikusabb ötleteket. Egy olyan hobbi lett ráadásul, amivel segíthetek másoknak elhagyni sok-sok műanyagot és szemetet. Most pedig ezt próbálom a szakmámmá formálni.
-        A rajzolás, grafika is csak egy pár éve lett az egyik kedvenc időtöltésem, hipp-hopp nagyon megszerettem a digitális rajzolást minden egyszerűségével és bonyolultságával együtt. A kedvenc témám itt is a természet, oda húz a kicsi szívem.
-        Próbálom a márkát is minél zöldebb útra terelni, mindenre természetes és környezettudatos verziót kitalálni.
-        Pls be nice és vegyél sok szép dolgot!
-      </p>
+      <h2 className="subtitle">varrott és rajzolt holmik</h2>
+      <p className="text">Maruzs Luca vagyok, a márka kitalálója, megalkotója, varrónője, grafikusa és mindenese.</p>
+      <p className="text">Évek óta álmodozom róla, hogy egyszer elinduljak ezen az úton, és végre azzal foglalkozzak, amit ennyire szeretek.</p>
+      <p className="text">Egy pár éve elkerülhetetlenné vált, hogy elkezdjek átváltani egy (legalább kicsit) környezettudatosabb életmódra. Itt jött be a képbe az alapvető személyiségjegyem, hogy „csináljunk meg mindent házilag, ha lehet”. Nagyjából egy éve nekikezdtem a varrásnak, azonnal megszerettem, és azóta is csak keresem és próbálgatom az újabb, hasznosabb és praktikusabb ötleteket. Egy olyan hobbi lett ráadásul, amivel segíthetek másoknak elhagyni sok-sok műanyagot és szemetet. Most pedig ezt próbálom a szakmámmá formálni.</p>
+      <p className="text">A rajzolás, grafika is csak egy pár éve lett az egyik kedvenc időtöltésem, hipp-hopp nagyon megszerettem a digitális rajzolást minden egyszerűségével és bonyolultságával együtt. A kedvenc témám itt is a természet, oda húz a kicsi szívem.</p>
+      <p className="text">Próbálom a márkát is minél zöldebb útra terelni, mindenre természetes és környezettudatos verziót kitalálni.</p>
+      <p className="text">Pls be nice és vegyél sok szép dolgot!</p>
     </div>
   );
 }
