e8ace6958f90fb7a33ed734e431e6626a0485a89
FluffyKas <maruzs.zso@gmail.com>
2022-06-30 13:02:31 +0200

    feat: add basic markup and styles for product page

--- src/pages/productPage/ProductPage.js
+++ src/pages/productPage/ProductPage.js
@@ -4,19 +4,25 @@ import "./ProductPage.scss";
 
 export const ProductPage = () => {
   return (
-    <div className="product-container">
-      <div className="product-descr">
-        <img src={logo} alt="Marouge" />
-        <div className="details">
-          <div>
-            <h2>Termek neve</h2>
-            <p>Termek ara</p>
-            <h3>Mibul van, mire jo?</h3>
-            <p>Csupa termeszetes cucc. Jo mindenre \o/</p>
+    <div className="product-page">
+      <div className="product-container">
+        <div className="container-left">
+          <img src={logo} className="product-illustration" alt="Marouge" />
+          <div className="swatches">
+            <button className="swatch-img"><img src={logo} alt="test-img" /></button>
+            <button className="swatch-img"><img src={logo} alt="test-img" /></button>
+            <button className="swatch-img"><img src={logo} alt="test-img" /></button>
+            <button className="swatch-img"><img src={logo} alt="test-img" /></button>
           </div>
         </div>
+        <div className="container-right">
+          <h2>Termek neve</h2>
+          <p>Termek ara</p>
+          <h3>Mibul van, mire jo?</h3>
+          <p>Csupa termeszetes cucc. Jo mindenre \o/</p>
+          <Link to="/katalogus" className="details-btn">Vissza</Link>
+        </div>
       </div>
-      <Link to="/katalogus" className="details-btn">Vissza</Link>
     </div>
   );
 }
