ac0c6c0fa429a2dbb4e395ded70713fb736cf3bc
FluffyKas <maruzs.zso@gmail.com>
2022-06-30 13:09:04 +0200

    feat: text first draft, raw

--- src/pages/order/Order.js
+++ src/pages/order/Order.js
@@ -3,9 +3,21 @@ import "./Order.scss";
 export const Order = () => {
   return (
     <div className="order-container">
-      <h1>Rendeles modja</h1>
-      <p>Kulgyel zsozsot, a csomag galambbal erkezik 3 munkanapon belul. Easy.</p>
-      <p>Szallitasi dij: egy marek mag kozvetlenul a kezbesitonek.</p>
+      <h1>Rendelés</h1>
+      <p>A rendelés egyelőre e-mailben vagy Instagram üzenetben történik. Keress bizalommal, megbeszéljük a részleteket, fizetési és szállítási módot, aztán már fel is adom neked és napokon belül érkezik a kiválasztott holmi!</p>
+      <p>Mivel elég friss a márka és még tanulok-fejlődök, egyelőre 2 fizetési módot tudok ajánlani neked:</p>
+      <ul>
+        <li>utánvét</li>
+        <li>előre utalás bankszámlára</li>
+      </ul>
+      <p>A szállítási módok pedig a következők:</p>
+      <ul>
+        <li>Packeta 670 Ft-tól</li>
+        <li>Foxpost 1199 Ft-tól</li>
+        <li>postai csomagküldés (DRÁGA)</li>
+        <li>reméljük egyszer majd GLS</li>
+        <li>személyes átvétel Pécsen?</li>
+      </ul>
     </div>
   );
 }
